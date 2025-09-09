6e69be24e9bfc48aad108807cc35989d6069718e
kaiquejw <kaiquecapt@gmail.com>
2025-09-08 22:03:07 -0300

    Primeiro commit da Missão 3

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita a idade do usuário
+        Console.Write("Digite sua idade: ");
+        string input = Console.ReadLine();
+
+        // Converte a entrada para inteiro
+        int idade;
+        if (int.TryParse(input, out idade))
+        {
+            // Verifica a faixa etária
+            if (idade < 12)
+            {
+                Console.WriteLine("Criança");
+            }
+            else if (idade >= 12 && idade <= 17)
+            {
+                Console.WriteLine("Adolescente");
+            }
+            else
+            {
+                Console.WriteLine("Adulto");
+            }
+        }
+        else
+        {
+            // Caso a entrada não seja um número válido
+            Console.WriteLine("Idade inválida. Digite um número inteiro.");
+        }
+    }
+}
